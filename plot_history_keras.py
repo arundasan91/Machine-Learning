d178efae4c51d16e6e7d22801c3d3812b648fc8b
Arun Das <14352842+arundasan91@users.noreply.github.com>
2020-02-29 10:47:06 -0600

    Create plot_history_keras.py

---
+++ plot_history_keras.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    # Plot training & validation accuracy values
+    # Requires a history dictionary
+    
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
